afed4870565417711548a66ace0808d44f8e2a7e
Pix xiP <pix119@proton.me>
2026-01-21 18:24:03 +0800

    update the shasum

--- Formula/pssh.rb
+++ Formula/pssh.rb
@@ -2,7 +2,7 @@ class Pssh < Formula
   desc "TUI wrapper around SSH client with search"
   homepage "https://github.com/Pix-xiP/pssh"
   url "https://github.com/Pix-xiP/pssh/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "f4347f3e7ea4b6840ad0b592eee314bd74c25e1d08a607b90bbbd3610433bdee"
+  sha256 "8cd87f5090d5e0e749c3ad3b876cadd83fdbb7f2c3a23208aeb3b89e9dd13c65"
   license "MIT"
 
   depends_on "go" => :build
